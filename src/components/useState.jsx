3bd083f54a3804acd7a6db2665e867dc84c5d5f6
renannscm <atomsfrontend@gmail.com>
2024-01-05 15:25:47 -0300

    feat: add new state and insert your value in document of page

--- src/components/useState.jsx
+++ src/components/useState.jsx
@@ -2,16 +2,19 @@ import { useState } from "react"
 
 export default function UseState() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('Renan')
 
     const IncrementCount = () => {
         // A forma recomendada de atualizar o estado é usando uma função que recebe o estado anterior e retorna o novo estado.
         setCount((prevState) => prevState + 1);
+        setName("Renan Nascimento da Silva")
     }
 
     return (
         <div>
             <h1>useState</h1>
-            <h2>0</h2>
+            <h2>{count}</h2>
+            <p>{name}</p>
             <button onClick={IncrementCount}>Increment</button>
         </div>
     )
